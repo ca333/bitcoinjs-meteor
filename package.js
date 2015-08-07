3077613ac3d3ca5f1b8f73fd5cd36bf05fafb977
ca333 <ca333@users.noreply.github.com>
2015-08-07 11:11:59 +0200

    limited binding @server

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ca333:bitcoinjs',
-  version: '1.5.7',
+  version: '1.5.9',
   summary: 'Pure JS bitcoin library',
   git: 'https://github.com/ca333/bitcoinjs-lib',
   documentation: 'README.md'
@@ -11,11 +11,11 @@ Npm.depends({'bitcoinjs-lib': '1.5.7'});
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.export('bitcoinjs');
-  api.addFiles('bitcoinjs.js');
+  api.addFiles('bitcoinjs.js',"server");
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('ca333:bitcoinjs');
-  api.addFiles('bitcoinjs-tests.js');
+  api.addFiles('bitcoinjs-tests.js',"server");
 });
